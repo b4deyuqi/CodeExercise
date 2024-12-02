1ba8e83a1548f72806771f8f2d96a6df8543ca34
b4deyuqi <b4deyuqi@gmail.com>
2024-12-02 15:31:29 +0800

    add notes

--- leetcode/11-container-with-most-water/main.cpp
+++ leetcode/11-container-with-most-water/main.cpp
@@ -4,6 +4,10 @@ using namespace std;
 
 int maxArea(vector<int> &height)
 {
+    /* 双指针 Time: O(N) Space: O(1)*/
+    /* 核心思路：相向双指针从两头向中间移动，直到两个指针相遇 */
+    /* 【关键点】移动哪个，怎么移动：移动左右指针中短的那个，总容量由量指针的距离和最短板子的高度决定。*/
+
     int l = 0, r = height.size() - 1;
     int maxv = 0, curv = 0;
     ;
